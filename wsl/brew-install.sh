44a3744bfff0e96e1273daed2eb6f964affe8899
Stanislav Musil <musil@sm-net.cz>
2022-09-01 22:16:55 +0200

    feat: wsl brew install

---
+++ wsl/brew-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew install kubectx
+brew install kn
+brew install kp
+brew install mkcert
+brew install nss
+brew install ytt
+brew install vmware-tanzu/tanzu/tanzu-community-edition
+brew install starship
+brew install kompose
