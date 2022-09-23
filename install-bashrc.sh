26e1ae5a3ec23745d0f6a2e379aeac43553870b3
Stanislav Musil <musil@sm-net.cz>
2022-09-23 19:53:09 +0200

    chore: echo

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -3,14 +3,24 @@
 timestamp=`date +%s`;
 
 function move(){
-mv -f ~/.bashrc ~/.bashrc.$timestamp
+    echo "Backing up .bashrc to .bashrc.$timestamp";
+    mv -f ~/.bashrc ~/.bashrc.$timestamp
 }
 
 function copy(){
+    echo "Copying .bashrc to ~";
+ 
 cp -f ./linux/bashrc ~/.bashrc
 }
 
 move
 copy
+
+# Bootstrap oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  mkdir -p ~/.oh-my-zsh/custom/plugins/eddiezane
+  mkdir -p ~/.oh-my-zsh/custom/themes
+fi
 
 echo "All done!"
