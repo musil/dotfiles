4a7cf8d187b76087123cecd2910cd560db2372a7
Stanislav Musil <musil@sm-net.cz>
2022-10-15 16:09:08 +0200

    chore: rename directory

--- brew/brew-install.sh
+++ brew/brew-install.sh
@@ -10,5 +10,13 @@ brew install vmware-tanzu/tanzu/tanzu-community-edition
 brew install starship
 brew install kompose
 brew install argocd
-#brew install minikube
+# brew install minikube
 brew isntall youtube-dl
+brew install monolith
+# brew install go
+# brew install openssl
+# brew install tree
+# brew install wget
+# brew install zsh
+# brew install imagemagick
+brew install ssh-copy-id
