96e6776bd02e4e9c402f096352889fbe7415be78
Stanislav Musil <musil@sm-net.cz>
2024-10-27 08:41:34 +0100

    fix: Save screenshots in PNG format

--- mac/defaults.sh
+++ mac/defaults.sh
@@ -8,7 +8,7 @@ defaults write com.apple.screencapture location ~/Desktop/screenshots
 defaults write com.apple.screencapture name "screenshot"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture name png
+defaults write com.apple.screencapture type png
 
 # Show download history even if you clear your browser history and cache.
 sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select LSQuarantineDataURLString from LSQuarantineEvent'
